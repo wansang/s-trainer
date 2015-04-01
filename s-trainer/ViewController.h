8ec7624cacde6288cc4ee3e8d0584de48c8f3217
junbinlim <jblim@smcore.co.kr>
2015-04-01 16:11:04 +0900

    Initial Commit

---
+++ s-trainer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  s-trainer
+//
+//  Created by Junbin Lim on 2015. 4. 1..
+//  Copyright (c) 2015년 prochild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ s-trainer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  s-trainer
+//
+//  Created by Junbin Lim on 2015. 4. 1..
+//  Copyright (c) 2015년 prochild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
